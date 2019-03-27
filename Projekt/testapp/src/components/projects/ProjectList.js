7e73440405b0cbd91ba8c1c0219550a556d645a3
Qiuqiong Zhou <qiuqiongzhou@admins-MacBook-Pro.local>
2019-03-27 23:17:42 +0100

    AddedDashboard

---
+++ Projekt/testapp/src/components/projects/ProjectList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return(
+       <div className="project-list-section">
+
+          <div className="card z-depth-0 project-summary">
+             <div className="card-contentgrey-text txt-dark-3">
+                <span className="card-title">Project Tilte</span>
+                <p>Posted by me-QQ</p>
+                <p className="grey-text">27 Mars, 2019</p>
+            </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+             <div className="card-contentgrey-text txt-dark-3">
+                <span className="card-title">Project Tilte</span>
+                <p>Posted by me-QQ</p>
+                <p className="grey-text">27 Mars, 2019</p>
+            </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+             <div className="card-contentgrey-text txt-dark-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by me-QQ</p>
+                <p className="grey-text">27 Mars, 2019</p>
+            </div>
+            </div>
+
+         </div>
+        )
+    }
+
+    export default ProjectList
