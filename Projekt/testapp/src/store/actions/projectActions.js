126e9712235f77ad07ee45dc4d5d55efd844fcbf
Qiuqiong Zhou <qiuqiongzhou@admins-MacBook-Pro.local>
2019-04-04 22:24:55 +0200

    AddedfbConfig

--- Projekt/testapp/src/store/actions/projectActions.js
+++ Projekt/testapp/src/store/actions/projectActions.js
@@ -6,8 +6,20 @@ export const createProject = (project) => {
     // project: 'project'
      //}
     
-     return (dispatch, getState) => {
-         // Att göra async till database
-         dispatch({ type: 'CREATE_PROJECT', project });
-     }
-};
+     return (dispatch, getState, { getFirebase, getFirestore }) => {
+         // Att göra async till database/make async call to database
+         const firestore = getFirestore();
+         firestore.collection('project').add({
+             ...project,
+             beltFirstName: 'Zhou',
+             beltLastName: 'Bella',
+             beltId: 11008,
+             createdAt: new Date()
+         }).then(() => {
+             dispatch({ type: 'CREATE_PROJECT', project }); 
+         }).catch((err) => {
+             dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+         
+         })       
+      }
+  };
